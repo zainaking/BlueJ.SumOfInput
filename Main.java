4fd283e24d8793ed53084a564a84df9d158fd06a
Zaina Cruz-King <zainacruz-king@zipcoders-MacBook-Pro.local>
2019-02-07 20:11:28 -0500

    whatever

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
+     
+
+   
 
     public static void main(String[] args){
+      int a=0;
+      
+     
+      Scanner in = new Scanner(System.in);
+     
+      System.out.println(" write number");
+      a = in.nextInt();
+      System.out.println(a+1);
+      
+      
+      
+        
+        
 
     }
 }
